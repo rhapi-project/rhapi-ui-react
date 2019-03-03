8a946075034b3243186c1abbe7a961bdafd4f6a4
rhapi <fabrice.duballet@lambdasoft.fr>
2019-03-03 13:54:48 +0100

    Update index.js

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import "semantic-ui-css/semantic.css";
 
-import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
