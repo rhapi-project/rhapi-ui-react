a3c4773fed997668fee4039af8fea76fb8006ced
dndayishima <divin.ndayishima@outlook.com>
2019-07-03 17:48:51 +0200

    Update exemples/*

--- exemples/src/Actes/Favoris.js
+++ exemples/src/Actes/Favoris.js
@@ -3,12 +3,17 @@ import { Client } from "rhapi-client";
 import { Actes } from "rhapi-ui-react";
 import { Button, Divider } from "semantic-ui-react";
 
+import _ from "lodash";
+
 // Instanciation du client RHAPI sans authentification
 const client = new Client("https://demo.rhapi.net/demo01");
 
 export default class ActesFavoris extends React.Component {
   componentWillMount() {
-    this.setState({ openFavoris: false });
+    this.setState({
+      openFavoris: false,
+      selectedActe: {}
+    });
   };
   render() {
     return (
@@ -25,7 +30,19 @@ export default class ActesFavoris extends React.Component {
           client={client}
           open={this.state.openFavoris}
           onClose={() => this.setState({ openFavoris: false })}
+          onSelection={(index, acte) => this.setState({ selectedActe: acte })}
         />
+        <Divider hidden={true} />
+        {!_.isEmpty(this.state.selectedActe)
+          ? <div>
+              Acte sélectionné à partir des actes en Favoris : <br />
+              Description : {this.state.selectedActe.description} <br />
+              Code : {this.state.selectedActe.code} <br />
+              Cotation : {this.state.selectedActe.cotation} <br />
+              Montant : {this.state.selectedActe.montant} <br />
+            </div>
+          : null
+        }
       </React.Fragment>
     )
   }
