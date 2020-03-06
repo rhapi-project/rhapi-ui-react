34639a16f76e1812fabf2eae6fbef2cde9189d8b
dndayishima <topndiv@gmail.com>
2020-03-06 15:37:27 +0100

    Handle download binary files

--- src/Components/Functions/index.js
+++ src/Components/Functions/index.js
@@ -2,6 +2,15 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 class Functions {
+  BinaryFiles = {
+    download: (base64, filename) => {
+      let a = document.createElement("a");
+      a.href = base64;
+      a.download = filename;
+      a.click();
+    }
+  };
+
   PDF = {
     download: (content, filename) => {
       let doc = new jsPDF();
@@ -13,7 +22,7 @@ class Functions {
     output: content => {
       let doc = new jsPDF();
       doc.fromHTML(content, 15, 5, {});
-      let base64 = doc.output('datauristring', { filename: "output.pdf" });
+      let base64 = doc.output("datauristring", { filename: "output.pdf" });
       let pdfWindow = window.open("#", "_blank");
       pdfWindow.document.write(
         "<iframe width='100%' height='100%' src='" + base64 + "'></iframe>"
@@ -34,11 +43,11 @@ class Functions {
         },*/
         /*allowTaint: true,*/
         onrendered: canvas => {
-            const imgData = canvas.toDataURL('image/png');
-            const docu = new jsPDF('p', 'mm', [297, 210]); //210mm wide and 297mm high - A4
-            
-            docu.addImage(imgData, 'PNG', 10, 10);
-            docu.save(filename + ".pdf");
+          const imgData = canvas.toDataURL("image/png");
+          const docu = new jsPDF("p", "mm", [297, 210]); //210mm wide and 297mm high - A4
+
+          docu.addImage(imgData, "PNG", 10, 10);
+          docu.save(filename + ".pdf");
         }
       });
 
